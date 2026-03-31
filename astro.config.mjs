f7fb0977a2f32aaafac740c303c3931e55d5acb2
vkumar0120 <vkumar0127@gmail.com>
2026-04-01 00:43:52 +0530

    added 5 files additionally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'static',
-  site: 'https://agasthiyamahasivanaadijothidam.com'
+  site: 'https://naadiastroservice.com'
 });
