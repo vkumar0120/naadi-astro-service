7291b9f85d3df0120d79fc125788d717a8ab8f15
vkumar0120 <vkumar0127@gmail.com>
2026-04-01 01:13:44 +0530

    added configuration for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    sitemap({
-      filter: (page) => 
-        !page.includes('/404') && 
-        !page.includes('/draft') &&
-        !page.includes('/test')
-    })
-  ],
+  site: 'https://naadiastroservice.com',
   output: 'static',
-  site: 'https://naadiastroservice.com'
+  integrations: [
+    tailwind()
+  ]
 });
